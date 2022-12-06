fdc79b6b56ff8edf2fe19598220216ddb650d396
CotulbeaAdrian <cotulbeaadrianionut@gmail.com>
2022-12-06 23:57:47 +0200

    Starting with splash screen

--- app/src/main/java/com/example/medbuddy/MainActivity.kt
+++ app/src/main/java/com/example/medbuddy/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // set splash screen then see if move to login or not
     }
 }
